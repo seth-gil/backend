61eac24a092399d8d313fdbe6a5b92be16819970
uood5 <sethdwoolley@gmail.com>
2019-04-18 21:37:10 +0100

    Added projects table

--- databaseSetup.py
+++ databaseSetup.py
@@ -4,3 +4,12 @@ import pymongo
 
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 
+mdb = client["mdb"]
+projects = mdb["mdb"]
+
+sample {"_id":"default",
+		"name":"name",
+		"description":"Sample project",
+		"Thumbnail":"root/default/1.jpg"}
+
+projects.insert_one(sample)
