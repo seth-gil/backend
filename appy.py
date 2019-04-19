89966fcd8245c49ba82a9d20fbbd6fecc25014a9
uood5 <sethdwoolley@gmail.com>
2019-04-19 14:31:38 +0100

    bugfixes

--- appy.py
+++ appy.py
@@ -87,6 +87,7 @@ def NewProject():
 	i = 0
 	for frame in frames:
 		#print (frame[20:])
+		print (frame)
 		image_data = re.sub('^data:image/.+;base64,', '', frame)
 		#print (image_data[20:])
 		im = Image.open(BytesIO(base64.b64decode(image_data)))
