c4a808f26ab01738068e00b8b5cb83f115e1d94c
uood5 <sethdwoolley@gmail.com>
2019-04-19 16:13:33 +0100

    Bugfixes

--- databaseSetup.py
+++ databaseSetup.py
@@ -7,11 +7,3 @@ client = pymongo.MongoClient("mongodb://localhost:27017/")
 mdb = client["mdb"]
 mdb["projects"].drop()
 projects = mdb["projects"]
-
-mdb["mdb"].drop()
-
-sample = {"_id":"default",
-		  "name":"name",
-		  "description":"Sample project"}
-
-projects.insert_one(sample)
