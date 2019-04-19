2abbb532597ca6106dc5d17c88e7a1e90832603d
uood5 <sethdwoolley@gmail.com>
2019-04-19 10:19:02 +0100

    Small changes

--- appy.py
+++ appy.py
@@ -54,7 +54,8 @@ def NewProject():
 
 	task_id = str(task_id)
 
-	print("\nNew project created\nName:%s\nDescription:\nFramerate:%s\nID:%s\n\n" % (name,desc,rate,task_id))
+	try: print("\nNew project created\nName:%s\nDescription:\nFramerate:%s\nID:%s\n\n" % (name,desc,rate,task_id))
+	except: print("ur print formatting sux") 
 
 	try:
 		request.files
