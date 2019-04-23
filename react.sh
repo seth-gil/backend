0b647f647b614b58aa2e951d3c5ad7975b275780
gilbertVirgo <gilbertjvirgo@gmail.com>
2019-04-23 22:58:57 +0100

    Edited react.sh

--- react.sh
+++ react.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+echo "Starting Frontend pull"
+
 cd /home/ec2-user/app/root
 rm -rf ./build ./precache-manifest* ./manifest.json ./asset-manifest.json ./static
 svn export https://github.com/seth-gil/frontend/trunk/build
 mv ./build/* ./
+
+echo "Pull successful"
